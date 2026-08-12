c01005746326dbdcaefd2c94d58674a7f4736bd8
mateo <mateo.preno18@gmail.com>
2026-08-12 09:10:34 -0300

    Stock actualizado a las 09:10:33

--- catalogo.js
+++ catalogo.js
@@ -75,7 +75,7 @@ const stock_actualizado = [
   { modelo: 'Vans Haylan grafite', talles: [{"talle": 43, "stock": 2}], foto: 'Fotos/Vans Haylan grafite.jpeg' },
   { modelo: 'Air forcé charol brillante', talles: [{"talle": 44, "stock": 1}], foto: 'Fotos/Air forcé charol brillante.jpeg' },
   { modelo: 'Glister beige pipa marrón', talles: [{"talle": 37, "stock": 2}, {"talle": 38, "stock": 1}], foto: 'Fotos/Glister beige pipa marrón.jpeg' },
-  { modelo: 'Vans Haylan 01 LUXO', talles: [{"talle": 40, "stock": 3}, {"talle": 41, "stock": 1}, {"talle": 42, "stock": 5}, {"talle": 43, "stock": 3}, {"talle": 44, "stock": 2}], foto: 'Fotos/Vans Haylan 01 LUXO.jpeg' },
+  { modelo: 'Vans Haylan 01 LUXO', talles: [{"talle": 40, "stock": 3}, {"talle": 42, "stock": 5}, {"talle": 43, "stock": 3}, {"talle": 44, "stock": 2}], foto: 'Fotos/Vans Haylan 01 LUXO.jpeg' },
   { modelo: 'Adidas 2000 royal', talles: [{"talle": 41, "stock": 1}, {"talle": 43, "stock": 1}], foto: 'Fotos/Adidas 2000 royal.jpeg' },
   { modelo: 'Vans Haylan marron', talles: [{"talle": 38, "stock": 3}, {"talle": 39, "stock": 1}], foto: 'Fotos/Vans Haylan marron.jpeg' },
   { modelo: 'ABZORB negras', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 1}, {"talle": 37, "stock": 3}], foto: 'Fotos/ABZORB negras.jpeg' },
