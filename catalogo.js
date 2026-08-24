2681cdf84e8f96b9e22fca45827fd8657746ab2f
mateo <mateo.preno18@gmail.com>
2026-08-24 17:02:56 -0300

    subo todo

--- catalogo.js
+++ catalogo.js
@@ -1,4 +1,4 @@
-const stock_actualizado = [
+const stock_zapatillas = [
   { modelo: 'Remera adidas blanca', talles: [{"talle": "S", "stock": 0}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/Remera adidas blanca.jpeg' },
   { modelo: 'Remera adidas negra', talles: [{"talle": "S", "stock": 0}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/Remera adidas negra.jpeg' },
   { modelo: 'baggy NK BORDADO gris', talles: [{"talle": "S", "stock": 0}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/baggy NK BORDADO gris.jpeg' },
