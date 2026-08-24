dd9b5a5b0d14a8bc0d8167879a911596a037e6f4
mateo <mateo.preno18@gmail.com>
2026-08-24 17:42:17 -0300

    Stock actualizado (zapatillas + indumentaria) a las 17:42:15

--- indumentaria.js
+++ indumentaria.js
@@ -1,6 +1,6 @@
 const stock_indumentaria = [
   { modelo: 'Remera adidas blanca', talles: [{"talle": "S", "stock": 1}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 1}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/Remera adidas blanca.jpeg' },
-  { modelo: 'Remera adidas negra', talles: [{"talle": "S", "stock": 0}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/Remera adidas negra.jpeg' },
+  { modelo: 'Remera adidas negra', talles: [{"talle": "S", "stock": 50}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 0}], foto: 'Fotos/Remera adidas negra.jpeg' },
   { modelo: 'baggy NK BORDADO gris', talles: [{"talle": "S", "stock": 1}, {"talle": "M", "stock": 0}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 0}, {"talle": "XXL", "stock": 1}], foto: 'Fotos/baggy NK BORDADO gris.jpeg' },
   { modelo: 'baggy NK BORDADO negro', talles: [{"talle": "S", "stock": 1}, {"talle": "M", "stock": 1}, {"talle": "L", "stock": 0}, {"talle": "XL", "stock": 1}, {"talle": "XXL", "stock": 1}], foto: 'Fotos/baggy NK BORDADO negro.jpeg' }
 ];
