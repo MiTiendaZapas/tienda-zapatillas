4fa5e3c8c85e25d9367b93305347b393f403d0a8
mateo <mateo.preno18@gmail.com>
2026-09-01 12:18:45 -0300

    Stock actualizado (zapatillas + indumentaria) a las 12:18:43

--- zapatillas_manual.js
+++ zapatillas_manual.js
@@ -8,6 +8,6 @@ const stock_zapatillas_manual = [
     { modelo: 'Nike super star', talles: [{"talle": 38, "stock": 1}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 3}, {"talle": 41, "stock": 3}, {"talle": 42, "stock": 1}, {"talle": 43, "stock": 1}], foto: 'Fotos/Nike super star.jpeg' },
     { modelo: 'Jordan 1 botitas charol negra', talles: [{"talle": 35, "stock": 1}, {"talle": 36, "stock": 2}, {"talle": 37, "stock": 3}, {"talle": 38, "stock": 3}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 1}], foto: 'Fotos/Jordan 1 botitas charol negra.jpeg'},
 
-    { modelo: 'Botines total 90', talles: [{"talle": 38, "stock": 1}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 2}, {"talle": 42, "stock": 2}], foto: 'Fotos/Botines total 90.jpeg'},
+    { modelo: 'Botines total 90', talles: [{"talle": 38, "stock": 1}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 2}, {"talle": 42, "stock": 1}], foto: 'Fotos/Botines total 90.jpeg'},
 
 ];
