7e18dbf8d246647bb9568fe83c3abc478bcd9762
mateo <mateo.preno18@gmail.com>
2026-09-02 18:47:10 -0300

    Stock actualizado (zapatillas + indumentaria) a las 18:47:08

--- zapatillas_manual.js
+++ zapatillas_manual.js
@@ -9,7 +9,7 @@ const stock_zapatillas_manual = [
     { modelo: 'Jordan low vérmelo', talles: [{"talle": 36, "stock": 1}, {"talle": 39, "stock": 1}], foto: 'Fotos/Jordan low vérmelo.jpeg' },
     { modelo: 'Jordan retro 4 pink', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 1}], foto: 'Fotos/Jordan retro 4 pink.jpeg' },
     { modelo: 'NB 530 blanca negro', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 3}, {"talle": 38, "stock": 2}, {"talle": 39, "stock": 1}], foto: 'Fotos/NB 530 blanca negro.jpeg' },
-    { modelo: 'New balance 9060 nova', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 3}, {"talle": 38, "stock": 2}, {"talle": 39, "stock": 1}], foto: 'Fotos/New balance 9060 nova.jpeg' },
+    { modelo: 'New balance 9060 nova', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 2}, {"talle": 38, "stock": 2}, {"talle": 39, "stock": 1}], foto: 'Fotos/New balance 9060 nova.jpeg' },
     { modelo: 'Panda super star', talles: [{"talle": 38, "stock": 0}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 3}, {"talle": 41, "stock": 3}, {"talle": 42, "stock": 2}, {"talle": 43, "stock": 1}], foto: 'Fotos/Panda super star.jpeg' },
     { modelo: 'Vans haylan marron blanco', talles: [{"talle": 41, "stock": 2}], foto: 'Fotos/Vans haylan marron blanco.jpeg' },
     { modelo: 'Vans Haylan total black', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 3}, {"talle": 39, "stock": 1}], foto: 'Fotos/Vans Haylan total black.jpeg' },
