646b812be54e3e07e51d2988c2d705225c069415
mateo <mateo.preno18@gmail.com>
2026-09-08 10:12:45 -0300

    Stock actualizado (zapatillas + indumentaria) a las 10:12:37

--- catalogo.js
+++ catalogo.js
@@ -57,7 +57,7 @@ const stock_zapatillas = [
   { modelo: 'Jordan 1 azules', talles: [{"talle": 34, "stock": 2}, {"talle": 35, "stock": 4}, {"talle": 36, "stock": 6}, {"talle": 37, "stock": 6}, {"talle": 38, "stock": 4}, {"talle": 39, "stock": 2}], foto: 'Fotos/Jordan 1 azules.jpeg' },
   { modelo: 'Jordan low brillo', talles: [{"talle": 35, "stock": 1}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 5}, {"talle": 38, "stock": 5}, {"talle": 39, "stock": 4}, {"talle": 40, "stock": 1}], foto: 'Fotos/Jordan low brillo.jpeg' },
   { modelo: 'ABZORB marrones', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 4}, {"talle": 36, "stock": 7}], foto: 'Fotos/ABZORB marrones.jpeg' },
-  { modelo: 'Shox tapón rojo', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 7}, {"talle": 41, "stock": 10}, {"talle": 42, "stock": 6}, {"talle": 43, "stock": 6}, {"talle": 44, "stock": 2}], foto: 'Fotos/Shox tapón rojo.jpeg' },
+  { modelo: 'Shox tapón rojo', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 7}, {"talle": 41, "stock": 10}, {"talle": 42, "stock": 5}, {"talle": 43, "stock": 6}, {"talle": 44, "stock": 2}], foto: 'Fotos/Shox tapón rojo.jpeg' },
   { modelo: 'Mind negras', talles: [{"talle": 43, "stock": 12}, {"talle": 44, "stock": 12}], foto: 'Fotos/Mind negras.jpeg' },
   { modelo: 'Jordan low pombo', talles: [{"talle": 36, "stock": 1}], foto: 'Fotos/Jordan low pombo.jpeg' },
   { modelo: 'Air Jordan pipa gris', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 37, "stock": 1}, {"talle": 38, "stock": 1}, {"talle": 39, "stock": 2}, {"talle": 40, "stock": 3}, {"talle": 41, "stock": 3}, {"talle": 42, "stock": 3}, {"talle": 43, "stock": 1}], foto: 'Fotos/Air Jordan pipa gris.jpeg' },
