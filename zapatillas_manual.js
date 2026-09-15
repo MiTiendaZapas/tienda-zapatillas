e67e49d234c6f291ac35dbd076ce1cf17310a31c
mateo <mateo.preno18@gmail.com>
2026-09-15 12:59:09 -0300

    Stock actualizado (zapatillas + indumentaria) a las 12:59:07

--- zapatillas_manual.js
+++ zapatillas_manual.js
@@ -2,16 +2,20 @@
 // nunca escribe ni pisa este archivo). Editado desde el Panel Admin.
 
 const stock_zapatillas_manual = [
-    { modelo: 'Adidas supernova', talles: [{"talle": 35, "stock": 1}, {"talle": 36, "stock": 1}, {"talle": 39, "stock": 0}], foto: 'Fotos/Adidas supernova.jpeg' },
-    { modelo: 'Botines total 90', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 2}, {"talle": 38, "stock": 1}], foto: 'Fotos/Botines total 90.jpeg' },
-    { modelo: 'Jordan 1 botitas charol negra', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 0}, {"talle": 36, "stock": 2}, {"talle": 37, "stock": 2}, {"talle": 38, "stock": 1}], foto: 'Fotos/Jordan 1 botitas charol negra.jpeg' },
+    { modelo: 'Adidas supernova', talles: [{"talle": 35, "stock": 1}, {"talle": 39, "stock": 1}], foto: 'Fotos/Adidas supernova.jpeg' },
+    { modelo: 'Botines total 90', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 1}], foto: 'Fotos/Botines total 90.jpeg' },
+    { modelo: 'Jordan 1 botitas charol negra', talles: [{"talle": 36, "stock": 2}, {"talle": 37, "stock": 2}, {"talle": 38, "stock": 1}], foto: 'Fotos/Jordan 1 botitas charol negra.jpeg' },
+    { modelo: 'Jordan 1 botitas pink', talles: [{"talle": 35, "stock": 1}, {"talle": 40, "stock": 1}], foto: 'Fotos/Jordan 1 botitas pink.jpeg' },
     { modelo: 'Jordan charol azul', talles: [{"talle": 42, "stock": 2}, {"talle": 43, "stock": 2}], foto: 'Fotos/Jordan charol azul.jpeg' },
+    { modelo: 'Jordan low pombo', talles: [{"talle": 42, "stock": 1}], foto: 'Fotos/Jordan low pombo.jpeg' },
     { modelo: 'Jordan low vérmelo', talles: [{"talle": 36, "stock": 1}, {"talle": 39, "stock": 1}], foto: 'Fotos/Jordan low vérmelo.jpeg' },
-    { modelo: 'Jordan retro 4 /total black', talles: [{"talle": 42, "stock": 1}, {"talle": 43, "stock": 3}, {"talle": 44, "stock": 1}], foto: 'Fotos/Jordan retro 4 total black.jpeg' },
+    { modelo: 'Jordan retro 4 /total black', talles: [{"talle": 42, "stock": 1}, {"talle": 43, "stock": 3}], foto: 'Fotos/Jordan retro 4 total black.jpeg' },
     { modelo: 'Jordan retro 4 pink', talles: [{"talle": 39, "stock": 1}, {"talle": 40, "stock": 1}], foto: 'Fotos/Jordan retro 4 pink.jpeg' },
-    { modelo: 'NB 530 blanca/negro', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 1}, {"talle": 36, "stock": 0}, {"talle": 37, "stock": 3}, {"talle": 38, "stock": 1}, {"talle": 39, "stock": 1}], foto: 'Fotos/NB 530 blanca negro.jpeg' },
+    { modelo: 'Mind beige', talles: [{"talle": "41/42", "stock": 1}, {"talle": "43/44", "stock": 1}], foto: 'Fotos/Mind beige.jpeg' },
+    { modelo: 'NB 530 blanca/negro', talles: [{"talle": 34, "stock": 1}, {"talle": 37, "stock": 3}, {"talle": 38, "stock": 1}], foto: 'Fotos/NB 530 blanca negro.jpeg' },
     { modelo: 'New balance 9060 nova', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 2}, {"talle": 38, "stock": 2}, {"talle": 39, "stock": 0}], foto: 'Fotos/New balance 9060 nova.jpeg' },
-    { modelo: 'Panda super star', talles: [{"talle": 39, "stock": 2}, {"talle": 41, "stock": 2}, {"talle": 42, "stock": 2}, {"talle": 43, "stock": 1}], foto: 'Fotos/Panda super star.jpeg' },
+    { modelo: 'Panda super star', talles: [{"talle": 41, "stock": 2}, {"talle": 42, "stock": 2}, {"talle": 43, "stock": 1}], foto: 'Fotos/Panda super star.jpeg' },
+    { modelo: 'Sb Jordan triple black', talles: [{"talle": 37, "stock": 1}], foto: 'Fotos/Sb Jordan triple black.jpeg' },
     { modelo: 'Vans haylan marron blanco', talles: [{"talle": 41, "stock": 0}], foto: 'Fotos/Vans haylan marron blanco.jpeg' },
-    { modelo: 'Vans Haylan total black', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 3}, {"talle": 39, "stock": 1}], foto: 'Fotos/Vans Haylan total black.jpeg' },
+    { modelo: 'Vans Haylan total black', talles: [{"talle": 34, "stock": 1}, {"talle": 35, "stock": 2}, {"talle": 36, "stock": 3}, {"talle": 37, "stock": 3}], foto: 'Fotos/Vans Haylan total black.jpeg' },
 ];
